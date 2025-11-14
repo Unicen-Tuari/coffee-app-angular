7fa1807495bc2cb76a7bd3a0b5518247c53de651
Ignacio Jonas <ignaciojonas@gmail.com>
2025-11-13 21:34:45 -0300

    Fix trailing slash in production API URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://coffee-shop-api-lndm.onrender.com/' // Update this with your production API URL
+  apiUrl: 'https://coffee-shop-api-lndm.onrender.com' // Update this with your production API URL
 };
